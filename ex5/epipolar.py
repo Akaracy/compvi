0f6f8ae3ef0ba48489b82415ce028981ff9cd5f9
Alexia <caravaca.alexia@gmail.com>
2026-06-06 09:39:21 +0200

    adding exercise 5 repo

---
+++ ex5/epipolar.py
@@ -0,0 +1,83 @@
+import numpy as np
+import cv2 as cv
+import glob
+from matplotlib import pyplot as plt
+ 
+images =sorted(glob.glob('leopard_pics/*.jpg'))
+
+img1 = cv.imread(images[2], cv.IMREAD_GRAYSCALE)  #queryimage # left image
+img2 = cv.imread(images[3], cv.IMREAD_GRAYSCALE) #trainimage # right image
+ 
+sift = cv.SIFT_create()
+ 
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+ 
+# FLANN parameters
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)
+ 
+flann = cv.FlannBasedMatcher(index_params,search_params)
+matches = flann.knnMatch(des1,des2,k=2)
+ 
+pts1 = []
+pts2 = []
+ 
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.8*n.distance:
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+print(len(pts1), "matches found for first images")
+print(len(pts2), "matches found for second images")
+
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+F, mask = cv.findFundamentalMat(pts1,pts2,cv.FM_LMEDS)
+print("Fundamental matrix is ", F)
+ 
+# We select only inlier points
+pts1 = pts1[mask.ravel()==1]
+pts2 = pts2[mask.ravel()==1]
+
+def drawlines(img1,img2,lines,pts1,pts2):
+    print('Drawing epilines on the images')
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    r,c = img1.shape
+    img1 = cv.cvtColor(img1,cv.COLOR_GRAY2BGR)
+    img2 = cv.cvtColor(img2,cv.COLOR_GRAY2BGR)
+    for r,pt1,pt2 in zip(lines,pts1,pts2):
+        color = tuple(np.random.randint(0,255,3).tolist())
+        x0,y0 = map(int, [0, -r[2]/r[1] ])
+        x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+        img1 = cv.line(img1, (x0,y0), (x1,y1), color,1)
+        img1 = cv.circle(img1,tuple(pt1),15,color,-1)
+        img2 = cv.circle(img2,tuple(pt2),15,color,-1)
+    return img1,img2
+
+# Find epilines corresponding to points in right image (second image) and
+# drawing its lines on left image
+lines1 = cv.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
+lines1 = lines1.reshape(-1,3)
+img5,img6 = drawlines(img1,img2,lines1,pts1,pts2)
+ 
+# Find epilines corresponding to points in left image (first image) and
+# drawing its lines on right image
+lines2 = cv.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
+lines2 = lines2.reshape(-1,3)
+img3,img4 = drawlines(img2,img1,lines2,pts2,pts1)
+
+print("Showing epilines for the first image")
+# Save the combined figure
+plt.subplot(121),plt.imshow(img5)
+plt.subplot(122),plt.imshow(img3)
+plt.savefig('epipolar_combined2.png', dpi=150, bbox_inches='tight')
+
+# Save individual images too
+cv.imwrite('epipolar_left2.png', img5)
+cv.imwrite('epipolar_right2.png', img3)
+
+print("Saved: epipolar_combined2.png, epipolar_left2.png, epipolar_right2.png")
